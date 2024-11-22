c7091a088546e45f3072e37ae295657a79422343
valeratrades <v79166789533@gmail.com>
2024-11-22 17:49:04 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use axum::{response::Html, routing::get, Router};
+mod market_structure;
+use market_structure::run;
 
 #[tokio::main]
 async fn main() {
+	run();
 	let app = Router::new().route("/", get(handler));
 
-	let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-		.await
-		.unwrap();
+	let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
 	println!("listening on {}", listener.local_addr().unwrap());
 	axum::serve(listener, app).await.unwrap();
 }
