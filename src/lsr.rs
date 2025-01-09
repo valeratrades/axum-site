48344e972dffed175e17464579f115575ef78340
valeratrades <valeratrades@gmail.com>
2025-01-09 23:33:08 +0000

    next: reimplement lsr from btc_line[Windows], get correct text print in isolation

--- src/lsr.rs
+++ src/lsr.rs
@@ -1,3 +1,8 @@
-fn main() {
-	println!("hello world");
+use v_exchanges::Binance;
+
+#[tokio::main]
+async fn main() {
+	let bn = Binance::default();
+	let lsr = bn.global_lsr_account(("BTC", "USDT").into(), "5m".into(), 24 * 12 + 1, "Global".into()).await.unwrap();
+	dbg!(&lsr[..5]);
 }
