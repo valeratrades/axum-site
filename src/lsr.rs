0884d66a09f5c0301917b4a4f499c6d2ec0fc45c
valeratrades <valeratrades@gmail.com>
2025-01-14 22:09:05 +0000

    concurrent (poor man's watermelon)

--- src/lsr.rs
+++ src/lsr.rs
@@ -1,9 +1,9 @@
+use futures::future::join_all;
 use v_exchanges::{binance, prelude::*};
 
 #[tokio::main]
 async fn main() {
-	color_eyre::install().unwrap();
-	v_utils::utils::init_subscriber(v_utils::utils::LogDestination::xdg(env!("CARGO_PKG_NAME")));
+	v_utils::clientside!();
 
 	let tf = "5m".into();
 	let range = (24 * 12 + 1).into();
@@ -12,13 +12,27 @@ async fn main() {
 	let m = "Binance/Futures".into();
 	let pairs = bn.exchange_info(m).await.unwrap().usdt_pairs().collect::<Vec<_>>();
 
-	for p in pairs {
-		let lsr = bn.lsr(p, tf, range, "Global".into()).await.unwrap();
-		if lsr.len() == 0 {
-			tracing::info!("No data for {}", p);
-			continue;
+	let futures = pairs.iter().map(|p| {
+		let bn = bn.clone();
+		async move {
+			match bn.lsr(*p, tf, range, "Global".into()).await {
+				Ok(lsr) if !lsr.is_empty() => Some((p, lsr[0].long() - lsr[lsr.len() - 1].long())),
+				Ok(_) => {
+					tracing::info!("No data for {}", p);
+					None
+				}
+				Err(e) => {
+					tracing::error!("Error fetching data for {}: {:?}", p, e);
+					None
+				}
+			}
 		}
-		let diff = lsr[0].long() - lsr[lsr.len() - 1].long();
-		dbg!(&diff);
+	});
+	let results = join_all(futures).await;
+
+	for r in results.into_iter().flatten() {
+		let (pair, diff) = r;
+		dbg!(&pair, &diff);
 	}
 }
+
